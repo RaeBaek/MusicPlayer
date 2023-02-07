566619a60ca2228f7dd89b2ac65ef7890092d669
RaeHoon Baek <bmp117@naver.com>
2023-02-07 21:16:09 +0900

    네이버 boostcource <iOS 앱 프로그래밍> - 음원 재생기 애플리케이션 2) 1~4

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var progressSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
+        
+        print("button tapped")
+    }
+    
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print("slider value changed")
+    }
+    
 }
 
